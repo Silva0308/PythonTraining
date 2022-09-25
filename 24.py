509ce3a117306fa81d251f258c18d7443dd20d2e
Silva0308 <travinina_valery@mail.ru>
2022-09-25 14:28:21 +0300

    сумма от 1 до А

--- 24.py
+++ 24.py
@@ -1,5 +1,15 @@
 # 24. Найти кубы чисел от 1 до N
-n = int(input("Введите N"))
+n = int(input("Введите N: "))
 for i in range(n+1):
     print(i**3, end=' ')
+print()
 
+#25. Найти сумму чисел от 1 до А
+a = int(input("Введите A: "))
+sum = 0
+for i in range(a+1):
+    sum += i
+print(sum)
+print()
+
+# 26.
