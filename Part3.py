f4bc2bd7b47556f1dc56aa59dbe69d883956bd45
Silva0308 <travinina_valery@mail.ru>
2022-09-25 17:07:05 +0300

    сумма элементов массива

--- Part3.py
+++ Part3.py
@@ -5,3 +5,18 @@ l = [randint(0,10) for x in range(8)]
 print(l)
 
 #32 .Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+from random import randint
+array1 = [randint(0, 1) for x in range(8)]
+print(array1)
+
+#33. Задать массив из 12 элементов, заполненных числами из [0,9].
+# Найти сумму положительных/отрицательных элементов массива
+from random import randint
+array1 = [randint(-9, 9) for x in range(12)]
+print(array1)
+result = 0
+for i in range(len(array1)):
+    if array1[i] > 0:
+        result += array1[i]
+print(result)
