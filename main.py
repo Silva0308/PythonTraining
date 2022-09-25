60dfa4cdd3c642d7dddae59a1d238957f68059d7
Silva0308 <travinina_valery@mail.ru>
2022-09-25 14:16:58 +0300

    таблица квадратов до N

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # Дано число. Проверить кратно ли оно 7 и 23
 n = int(input())
-if n%7 and n%23:
+if n%7==0 and n%23==0:
     print("yes")
 else:
     print("no")
+
+#Показать таблицу квадратов чисел от 1 до N
+n = int(input("Введите N"))
+for i in range(n+1):
+    print(i**2, end=' ')
+
